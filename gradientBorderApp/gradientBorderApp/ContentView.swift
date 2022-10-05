4b294670caf02a3cd169ca422330f6cebaf1bbe6
youn9k <ace_lephant@naver.com>
2022-10-05 10:57:21 +0900

    [FIX] 여러개 생성되면 애니메이션이 동작안하던 버그
    some View 에서 View 로 변경

--- gradientBorderApp/gradientBorderApp/ContentView.swift
+++ gradientBorderApp/gradientBorderApp/ContentView.swift
@@ -8,8 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    private let gradientColors: [Color] = [.red, .yellow, .green, .blue, .purple, .red]
-    @State  var gradientAngle: Double = 0
     var body: some View {
         Image("IU")
             .resizable()
@@ -18,17 +16,7 @@ struct ContentView: View {
             .mask { RoundedRectangle(cornerRadius: 12) }
             .shadow(radius: 8)
             .overlay {
-                gradientBorderView
-            }
-    }
-    var gradientBorderView: some View {
-        RoundedRectangle(cornerRadius: 12)
-            .strokeBorder(
-                AngularGradient(gradient: Gradient(colors: gradientColors), center: .center, angle: .degrees(gradientAngle)))
-            .onAppear {
-                withAnimation(.linear(duration: 3).repeatForever(autoreverses: false)) {
-                    self.gradientAngle = 360
-                }
+                GradientBorderView()
             }
     }
 }
@@ -36,5 +24,21 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct GradientBorderView: View {
+    private var colors: [Color] = [.red, .yellow, .green, .blue, .purple, .red]
+    @State private var gradientAngle: Double = 0
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 12)
+            .strokeBorder(
+                AngularGradient(gradient: Gradient(colors: colors), center: .center, angle: .degrees(gradientAngle)))
+            .onAppear {
+                withAnimation(.linear(duration: 3).repeatForever(autoreverses: false)) {
+                    self.gradientAngle = 360
+                }
+            }
     }
 }
